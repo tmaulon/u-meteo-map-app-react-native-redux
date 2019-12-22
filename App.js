8f53aa4076d58ce74de700abece637f86f1c944d
tmaulon <thomas.maulon@gmail.com>
2019-12-22 18:41:21 +0100

    add Provider in App

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import SearchScreen from './screens/SearchScreen';
+import store from './store';
+import { Provider } from 'react-redux';
 
 export default class App extends React.Component {
 
 
   render() {
     return (
-      <SearchScreen />
+      <Provider store={store}>
+        <SearchScreen />
+      </Provider>
     );
   }
 }
