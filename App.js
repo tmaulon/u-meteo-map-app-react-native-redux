2de43b9d6a53b922278e7a884d831e07a57017d1
tmaulon <thomas.maulon@gmail.com>
2019-12-23 17:17:30 +0100

    remove useless spacing

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import store from './store';
 import { Provider } from 'react-redux';
 
 export default class App extends React.Component {
-
-
   render() {
     return (
       <Provider store={store}>
