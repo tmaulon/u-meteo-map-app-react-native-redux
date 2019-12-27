8b8e1907febdb34667719b5e84923ab2ab479785
tmaulon <thomas.maulon@gmail.com>
2019-12-28 00:07:27 +0100

    create indexScreen

---
+++ screens/index-screen.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Text, View, AsyncStorage } from 'react-native'
+import { connect } from 'react-redux'
+import { facebookLogin, setToken } from '../actions/index'
+import { withNavigation } from 'react-navigation'
+
+class IndexScreen extends Component {
+
+    componentDidMount() {
+        // verify if we already have a token
+        AsyncStorage.getItem("fbToken").then(
+            token => {
+                if (token) {
+                    this.props.setToken(token)
+                    this.goToSearch()
+                } else {
+                    this.props.facebookLogin(this.goToSearch)
+                }
+            }
+        )
+    }
+
+    goToSearch = () => {
+        this.props.navigation.push("Search")
+    }
+
+    render() {
+        return (
+            <View />
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {}
+}
+
+const mapDispatchToProps = {
+    facebookLogin,
+    setToken
+}
+
+export default withNavigation(connect(mapStateToProps, mapDispatchToProps)(IndexScreen));
