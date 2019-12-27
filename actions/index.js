23fc87e9c4c2e1828e130b04d3cf203c07286554
tmaulon <thomas.maulon@gmail.com>
2019-12-28 00:07:14 +0100

    add facebookLogin in actions

--- actions/index.js
+++ actions/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
-import { API_KEY } from '../constant';
-import { SET_CURRENT_WHEATHER } from './action-types';
-import { SET_FORECAST_WEATHER } from './action-types';
+import { API_KEY, FACEBOOK_APP_ID } from '../constant';
+import { SET_CURRENT_WHEATHER, SET_FORECAST_WEATHER, FACEBOOK_LOGIN_SUCCESS, FACEBOOK_LOGIN_ERROR } from './action-types';
 
 const WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
 const FORECAST_WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
+
+// import { Facebook } from 'expo'
+import * as Facebook from 'expo-facebook';
+import { AsyncStorage } from 'react-native';
 
 //action creator
 export const getCurrentWeatherByCity = city => async dispatch => {
@@ -17,4 +20,39 @@ export const getForecastWeatherByCity = city => async dispatch => {
     const response = await axios.get(`${FORECAST_WEATHER_BASE_URL}?q=${city}&appid=${API_KEY}`);
     // when dispatch() is called, all reducers are called
     dispatch({ type: SET_FORECAST_WEATHER, payload: response.data })
+}
+
+export const facebookLogin = (onSuccess, onError) => dispatch => {
+    Facebook.initializeAsync(FACEBOOK_APP_ID);
+    const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+    } = Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+    }).then(fbResponse => {
+        if (fbResponse.type === "success") {
+            //dispatcher onSuccess fbResponse.token
+            setToken(fbResponse.token)
+            // dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: fbResponse.token })
+            AsyncStorage.setItem("fbToken", fbResponse.token)
+            onSuccess && onSuccess()
+
+        } else {
+            //dispatcher une erreur
+            dispatch({ type: FACEBOOK_LOGIN_ERROR })
+            onError && onError()
+        }
+    }).catch(error => {
+        // dispatcher erreur
+        dispatch({ type: FACEBOOK_LOGIN_ERROR })
+        onError && onError()
+
+    })
+}
+
+export const setToken = (token) => dispatch => {
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token })
 }
