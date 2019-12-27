6570c13b58eb314e97aabac28615c8f7a01257f1
tmaulon <thomas.maulon@gmail.com>
2019-12-27 18:19:37 +0100

    pass city in advanced detail screen props

--- screens/advanced-detail-screen.js
+++ screens/advanced-detail-screen.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { connect } from 'react-redux'
+import { withNavigation } from 'react-navigation'
+import { getForecastWeatherByCity } from '../actions'
 
 export class AdvancedDetailScreen extends Component {
+
+    componentDidMount() {
+        const city = this.props.navigation.getParam("city");
+        this.props.getForecastWeatherByCity(city)
+    }
+
     render() {
         return (
             <View style={{ justifyContent: "center", alignItems: "center", height: "100%" }}>
@@ -15,6 +23,8 @@ export class AdvancedDetailScreen extends Component {
 const mapStateToProps = state => {
     return {};
 };
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+    getForecastWeatherByCity
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdvancedDetailScreen);
